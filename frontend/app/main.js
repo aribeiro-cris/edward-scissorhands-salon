2378a662f1e206fb0d10a9253f7fb40dcedca4a0
Andreia <andreiacribeiro.5@gmail.com>
2024-05-27 23:28:24 +0100

    appointment form done

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -76,7 +76,110 @@ function render(){
 }
 
 function form(container){
-    console.log("form")
+    const title = document.createElement("h1");
+    title.innerText = "Appointment with Edward";
+    container.appendChild(title);
+
+    const formDiv = document.createElement("div");
+    const form = document.createElement("form");
+    form.classList.add("form");
+    form.method = "post";
+
+    const name = document.createElement("label");
+    name.innerText = "Your name";
+    name.classList.add("form-label");
+
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.pattern = ".*\\S.*";
+    nameInput.title = "Please add your name.";
+    nameInput.required = true;
+    nameInput.minLength = 3;
+
+    form.appendChild(name);
+    form.appendChild(nameInput);
+
+    const phone = document.createElement("label");
+    phone.innerText = "Your phone number";
+    phone.classList.add("form-label");
+
+    const phoneInput = document.createElement("input");
+    phoneInput.type = "tel";
+    phoneInput.pattern = "[0-9]{9}";
+    phoneInput.title = "Please provide a valid phone number.";
+    phoneInput.required = true;
+
+    form.appendChild(phone);
+    form.appendChild(phoneInput);
+
+    const service = document.createElement("label");
+    service.innerText = "What services do you want?";
+    service.classList.add("form-label");
+
+    const serviceInput = document.createElement("select");
+    
+    const serviceInputOption1 = document.createElement("option");
+    serviceInputOption1.innerText = "Styling";
+
+    const serviceInputOption2 = document.createElement("option");
+    serviceInputOption2.innerText = "Coloring";
+
+    const serviceInputOption3 = document.createElement("option");
+    serviceInputOption3.innerText = "Haircut";
+
+    serviceInput.appendChild(serviceInputOption1);
+    serviceInput.appendChild(serviceInputOption2);
+    serviceInput.appendChild(serviceInputOption3);
+
+    form.appendChild(service);
+    form.appendChild(serviceInput);
+
+    const date = document.createElement("label");
+    date.innerText = "Time and date for your appointment with Mr. Edward";
+    date.classList.add("form-label");
+
+    const dateInput = document.createElement("input");
+    dateInput.type = "datetime-local";
+
+    //need to find alternative option to this
+    dateInput.addEventListener('input', function() {
+        const event = new Date(this.value);
+        const hours = event.getHours();
+        const mins = event.getMinutes();
+
+        if(hours >= 18 || hours < 10) {
+            alert("Appointment can only be booked between 10am and 6pm.");
+            //this.value = "";
+            return;
+        }
+
+        if(!mins == 30 || !mins == 0) 
+            {
+                alert("Appointment can only be booked every half hour.");
+                return;
+            }
+    });
+
+    form.appendChild(date);
+    form.appendChild(dateInput);
+
+    const comments = document.createElement("label");
+    comments.innerText = "Any comments that you want to share with Mr.Edward?";
+    comments.classList.add("form-label");
+
+    const commentsInput = document.createElement("input");
+    commentsInput.type = "text";    
+
+    form.appendChild(comments);
+    form.appendChild(commentsInput);
+
+    const submitForm = document.createElement("button");
+    submitForm.innerText = "Submit";
+
+    form.appendChild(submitForm);
+    formDiv.appendChild(form);
+
+    container.appendChild(formDiv);
 }
 
 function confirms(container){
